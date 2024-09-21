3b4fd975de833810d16ac1b3fe37d5cbf9ec2670
ArthurF_Ubuntu <Arthurfdarosa@gmail.com>
2024-09-20 23:21:14 -0300

    Feat typescript: Product Model methods update to TS

--- models/product.model.ts
+++ models/product.model.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb'
+import { ObjectId, DeleteResult } from 'mongodb'
 
 import * as db from '../data/database'
 
@@ -6,77 +6,81 @@ class CustomError extends Error {
   code?: number;
 }
 
-interface Iimage {
-  imagePath: string;
-  imageUrl: string;
-}
 
 interface IProduct {
-  _id?: string; // Optional _id
   title: string;
   summary: string;
   price: number;
   description: string;
-  image: Iimage;
+  image?: string;
+  imagePath?: string;
+  imageUrl?: string;
+  _id?: ObjectId; // Optional _id
 }
 
 class Product {
-  id?: string; // Optional id
-  title: string;
-  summary: string;
-  price: number;
-  description: string;
-  image: {
-    imagePath: string;
-    imageUrl: string;
-  }
+  public id?: ObjectId; // Optional id
+  public title: string;
+  public summary: string;
+  public price: number;
+  public description: string;
+  public image?;
+  public imagePath?: string;
+  public imageUrl?: string;
+
 
   constructor(productData: IProduct) {
-    this.title = productData.title
-    this.summary = productData.summary
-    this.price = +productData.price
-    this.description = productData.description
-    this.image = productData.image // the name of the image file
-    // this.updateImageData()
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = +productData.price;
+    this.description = productData.description;
+    this.image = productData.image; // the name of the image file
+    this.updateImageData();
     if (productData._id) {
-      this.id = productData._id
+      this.id = productData._id;
     }
   }
 
-  static async findById(productId: string): Promise<Product | null> {
-    let prodId: ObjectId
+  static async findById(productId: string): Promise<Product> {
+    let prodId: ObjectId;
     try {
-      prodId = new ObjectId(productId)
+      prodId = new ObjectId(productId);
     } catch (error: any) {
-      error.code = 404
-      throw error
+      error.code = 404;
+      throw error;
     }
+
     const product = await db
       .getDb()
-      .collection('products')
+      .collection<Product>('products')
       .findOne({ _id: prodId })
 
     if (!product) {
       const error = new CustomError('Could not find product with provided id.')
-      error.code = 404
-      throw error
+      error.code = 404;
+      throw error;
     }
 
     return new Product(product)
   }
 
   static async findAll(): Promise<Product[]> {
-    const products = await db.getDb().collection('products').find().toArray()
+    const products = await db
+      .getDb()
+      .collection('products')
+      .find()
+      .toArray()
 
-    return products.map((productDocument: IProduct) => new Product(productDocument))
+    return products.map((productDocument) => new Product(productDocument as IProduct))
   }
+
 
   static async findMultiple(ids: string[]): Promise<Product[]> {
     const productIds = ids.map(id => new ObjectId(id))
 
     const products = await db
       .getDb()
-      .collection('products')
+      .collection<IProduct>('products')
       .find({ _id: { $in: productIds } })
       .toArray()
 
@@ -84,8 +88,8 @@ class Product {
   }
 
   updateImageData(): void {
-    this.image.imagePath = `product-data/images/${this.image}`
-    this.image.imageUrl = `/products/assets/images/${this.image}`
+    this.imagePath = `product-data/images/${this.image}`
+    this.imageUrl = `/products/assets/images/${this.image}`
   }
 
   async save(): Promise<void> {
@@ -104,14 +108,20 @@ class Product {
         delete productData.image
       }
 
-      await db.getDb().collection('products').updateOne(
-        { _id: productId },
-        {
-          $set: productData
-        }
-      )
+      await db
+        .getDb()
+        .collection('products')
+        .updateOne(
+          { _id: productId },
+          {
+            $set: productData
+          }
+        )
     } else {
-      await db.getDb().collection('products').insertOne(productData)
+      await db
+        .getDb()
+        .collection('products')
+        .insertOne(productData)
     }
   }
 
@@ -120,8 +130,8 @@ class Product {
     this.updateImageData()
   }
 
-  remove(): Promise<void> {
-    const productId = new ObjectId(this.id)
+  remove(): Promise<DeleteResult> {
+    const productId = new ObjectId(this.id!)
     return db.getDb().collection('products').deleteOne({ _id: productId })
   }
 }
